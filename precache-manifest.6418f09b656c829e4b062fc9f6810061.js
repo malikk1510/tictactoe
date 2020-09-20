6b8f1b127a760d729a721b77cf4655983be3881d
malikk1510 <69068011+malikk1510@users.noreply.github.com>
2020-09-20 15:34:47 +0530

    Updates

---
+++ precache-manifest.6418f09b656c829e4b062fc9f6810061.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0362a9a4eaa9a6f38e7f22a93a22688",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "d0f1390f95e83569d35a",
+    "url": "/tictactoe/static/css/2.d4dfd184.chunk.css"
+  },
+  {
+    "revision": "f9e02850d74aba84be28",
+    "url": "/tictactoe/static/css/main.817eff5d.chunk.css"
+  },
+  {
+    "revision": "d0f1390f95e83569d35a",
+    "url": "/tictactoe/static/js/2.8df0f9bd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tictactoe/static/js/2.8df0f9bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e02850d74aba84be28",
+    "url": "/tictactoe/static/js/main.024cc167.chunk.js"
+  },
+  {
+    "revision": "6a312990031a7ee2695f",
+    "url": "/tictactoe/static/js/runtime-main.d68cccfb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tic-tac/precache-manifest.6e54db5155f2134e619564e8be087dde.js"
+  "/tictactoe/precache-manifest.6418f09b656c829e4b062fc9f6810061.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tictactoe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
